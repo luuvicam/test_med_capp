31ba6a37855aea6199dd6d8e5a50efecfaa459d9
“Cam <luuvicam@gmail.com>
2025-08-30 22:18:52 -0400

    login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
 import './Login.css';
 
 function Login() {
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState('');
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (sessionStorage.getItem("auth-token")) {
+            navigate("/");
+        }
+    }, []);
+    const login = async (e) => {
+        e.preventDefault();
+        const res = await fetch(`${API_URL}/api/auth/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+        });
+        const json = await res.json();
+        if (json.authtoken) {
+            // If authentication token is received, store it in session storage
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('email', email);
+            // Redirect to home page and reload the window
+            navigate('/');
+            window.location.reload();
+        } else {
+            // Handle errors if authentication fails
+            if (json.errors) {
+                for (const error of json.errors) {
+                    alert(error.msg);
+                }
+            } else {
+                alert(json.error);
+            }
+        }
+    };
     return (
         <div className="container">
             <div className="login-grid">
@@ -15,11 +58,12 @@ function Login() {
                     <form>
                         <div className="form-group">
                             <label for="email">Email</label>
-                            <input type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
+                            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
                         </div>
                         <div className="form-group">
                             <label for="password">Password</label>
                             <input
+                                value={password} onChange={(e) => setPassword(e.target.value)}
                                 type="password"
                                 name="password"
                                 id="password"
@@ -29,7 +73,7 @@ function Login() {
                             />
                         </div>
                         <div className="btn-group">
-                            <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Login</button>
+                            <button type="button" onClick={login} className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Login</button>
                             <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light">Reset</button>
                         </div>
                         <br />
