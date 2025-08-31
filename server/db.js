4d54e19f9069ed5513f0f83fc1fced38565749f1
“Cam <luuvicam@gmail.com>
2025-08-30 21:36:36 -0400

    structure app

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:4WvNRYO1ADMQEvCvElil6TF3@172.21.122.148:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
